ba7bead877c33c6fd40900c793a7afd262c9b07f
Sterling Walker <swalker3@oakland.edu>
2020-09-09 20:13:06 -0400

    Initial commit

---
+++ src/main/java/com/management/HumanResources/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.management.HumanResources;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HumanResourcesApplication.class);
+	}
+
+}
---
+++ src/test/java/com/management/HumanResources/HumanResourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.HumanResources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
