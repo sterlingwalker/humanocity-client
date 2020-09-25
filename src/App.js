c183abfe36213541eda1fc5c507b4cd37c66a7cb
Sterling <swalker3@oakland.edu>
2020-09-25 10:19:48 -0400

    Appbar and routing added, styling updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import { makeStyles } from '@material-ui/styles'
 import './App.css';
 import EmployeeTable from './components/EmployeeTable'
 import HomeCard from './components/HomeCard'
+import NavigationBar from './components/NavigationBar'
+import { Route } from 'react-router'
+import { withRouter } from 'react-router-dom'
+
+const useStyles = makeStyles({
+  cards: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridAutoRows: 'auto', 
+    gridGap: '1rem',
+    marginTop: '80px'
+  }
+});
 
 function App() {
-  return (
-    <div className="App">
-      
-      <EmployeeTable />
+  const classes = useStyles()
+
+  const cards = (
+    <div className={classes.cards}>
       <HomeCard icon={process.env.PUBLIC_URL + 'icons/person.svg'} title="Personal Time Off" description="Employees' requested time off" />
-      <br/>
-      <HomeCard icon={process.env.PUBLIC_URL + 'icons/people.svg'} title="User Management" description="Add, remove, and manage employees" />
-      <br/>
+      <HomeCard icon={process.env.PUBLIC_URL + 'icons/people.svg'} title="User Management" description="Add, remove, and manage employees" url={'/all/users'} />
       <HomeCard icon={process.env.PUBLIC_URL + 'icons/schedule.svg'} title="Schedule" description="Weekly employee schedule" />
-      <br/>
       <HomeCard icon={process.env.PUBLIC_URL + 'icons/feedback.svg'} title="Feedback" description="Employee complaints and feedback" />
+    </div>
+  )
+  return (
+    <div>
+    <NavigationBar />
+    <React.Fragment>
+      <Route path='/' exact render={() => cards} />
+      <Route path='/all/users' component={EmployeeTable} />
+    </React.Fragment>
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { useHistory } from 'react-router-dom'
 
 const useStyles = makeStyles({
   root: {
@@ -22,9 +21,11 @@ const useStyles = makeStyles({
 
 export default function HomeCard(props) {
   const classes = useStyles();
+  let history = useHistory();
+
 
   return (
-    <Card className={classes.root}>
+    <Card className={classes.root} onClick={() => history.push(props.url)}>
       <CardActionArea>
         <CardMedia
           className={classes.media}
