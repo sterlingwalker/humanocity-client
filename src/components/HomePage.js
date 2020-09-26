37c094fb3d2a904f1a68c922e49b67c1fa0b1ec8
Yonatan Mankovich <yonimn2000@gmail.com>
2020-09-25 21:38:02 -0400

    Create HomePage component.
    Moved the home page to a separate file to keep it organized.

---
+++ src/components/HomePage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import HomeCard from './HomeCard'
+
+const useStyles = makeStyles({
+    cards: {
+        display: 'grid',
+        gridTemplateColumns: 'repeat(3, 1fr)',
+        gridAutoRows: 'auto', 
+        gridGap: '1rem',
+        marginTop: '80px'
+      }
+});
+
+export default function HomePage() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.cards}>
+      <HomeCard icon={process.env.PUBLIC_URL + 'icons/person.svg'} title="Personal Time Off" description="Employees' requested time off" />
+      <HomeCard icon={process.env.PUBLIC_URL + 'icons/people.svg'} title="User Management" description="Add, remove, and manage employees" url={'/all/users'} />
+      <HomeCard icon={process.env.PUBLIC_URL + 'icons/schedule.svg'} title="Schedule" description="Weekly employee schedule" />
+      <HomeCard icon={process.env.PUBLIC_URL + 'icons/feedback.svg'} title="Feedback" description="Employee complaints and feedback" />
+    </div>
+  );
+}
