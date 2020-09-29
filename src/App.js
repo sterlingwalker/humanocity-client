cdc1b7a03671ae7e2170319e142fc96c22740b2a
Sterling <swalker3@oakland.edu>
2020-09-29 12:29:17 -0400

    Tab buttons wired up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import EmployeeTable from './components/EmployeeTable'
 import NavigationBar from './components/NavigationBar'
 import HomePage from './components/HomePage'
 import { Route } from 'react-router'
-import { withRouter } from 'react-router-dom'
+import { withRouter, useHistory } from 'react-router-dom'
 
 function App() {
+const [tab, setTab] = useState('/')
+let history = useHistory()
+
+useEffect(() =>
+  {
+    setTab(history.location.pathname)
+  },[history.location])
+
   return (
     <div>
-      <NavigationBar />
+      <NavigationBar currentTab={tab} />
       <React.Fragment>
         <Route path='/' exact component={HomePage} />
         <Route path='/all/users' component={EmployeeTable} />
