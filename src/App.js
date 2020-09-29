6438c70be4c9b06bc079bd6a7d25db1701ec8d68
Yonatan Mankovich <yonimn2000@gmail.com>
2020-09-29 16:21:56 -0400

    Add profile page.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import EmployeeTable from './components/EmployeeTable'
+import ProfilePage from './components/ProfilePage'
 import NavigationBar from './components/NavigationBar'
 import HomePage from './components/HomePage'
 import { Route } from 'react-router'
@@ -10,9 +11,11 @@ function App() {
   return (
     <div>
       <NavigationBar />
+      <div style={{marginTop: 5 +'em' /* Add spacing between navbar and page contents */}}></div>
       <React.Fragment>
         <Route path='/' exact component={HomePage} />
-        <Route path='/all/users' component={EmployeeTable} />
+        <Route path='/employee' exact component={ProfilePage} />
+        <Route path='/employees' component={EmployeeTable} />
       </React.Fragment>
     </div>
   );
