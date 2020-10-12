dea15799bfcf5ddecf2bd52930d92ba362206e5f
Dennes Donohue <71045080+dennes4878@users.noreply.github.com>
2020-10-12 14:29:30 -0400

    Created feedback page
    Created and wired up feedback page

--- src/components/FeedbackPage.js
+++ src/components/FeedbackPage.js
@@ -23,7 +23,7 @@ const useStyles = makeStyles((theme) => ({
   root: {
     '& .MuiTextField-root': {
       margin: theme.spacing(1),
-      marginRight: theme.spacing(50),
+      marginRight: theme.spacing(1),
       width: '25ch',
     },
   },
